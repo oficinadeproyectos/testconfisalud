041fcaff68aec087d3357b9dec581b500e1ab621
Scharss <scharss@gmail.com>
2020-01-31 10:26:46 -0500

    firebase

--- js/registro.js
+++ js/registro.js
@@ -1,69 +1,62 @@
 // Inicializar la base de datos
 var config = {
-    apiKey: "AIzaSyB73xrbbkWO5IiJcs8HR3vbdQJ171Sh6-w",
-    authDomain: "geoleaf-a1530.firebaseapp.com",
-    databaseURL: "https://geoleaf-a1530.firebaseio.com",
-    projectId: "geoleaf-a1530",
-    storageBucket: "geoleaf-a1530.appspot.com",
-    messagingSenderId: "719308462883"
+    apiKey: "AIzaSyCJBQBZr8lsxCOSe18-SOUw5VUI3n4x2-A",
+    authDomain: "confisalud-3e382.firebaseapp.com",
+    databaseURL: "https://confisalud-3e382.firebaseio.com",
+    projectId: "confisalud-3e382",
+    storageBucket: "confisalud-3e382.appspot.com",
+    messagingSenderId: "629122550546",
+    appId: "1:629122550546:web:0dad7ee8026377517c01cb",
+    measurementId: "G-PDTVJKVZ5H"
 };
 
 firebase.initializeApp(config);
 
-var email,password,passwordConfirm;
+var email, password, passwordConfirm;
 
-function exito()
-{
-  alert('Se ha creado la cuenta de usuario correctamente. ');
-  location.assign('index.html');
+function exito() {
+    alert('Se ha creado la cuenta de usuario correctamente. ');
+    location.assign('index.html');
 }
 
-function alFinalizar(error)
-{
+function alFinalizar(error) {
     // console.log(error);
 
-    if (error!=='undefined')
-    {
+    if (error !== 'undefined') {
         // Códigos de error:
         // auth/invalid-email
         // auth/weak-password
         // auth/email-already-in-use
-        switch(error.code)
-        {
+        switch (error.code) {
             case 'auth/email-already-in-use':
-            alert('ERROR: No se puede crear la nueva cuenta de usuario, por que el e-mail ya está en uso !');
-            break;
+                alert('ERROR: No se puede crear la nueva cuenta de usuario, por que el e-mail ya está en uso !');
+                break;
             case 'auth/invalid-email':
-            alert('ERROR: El e-mail facilitado no es un e-mail correcto.');
-            break;
+                alert('ERROR: El e-mail facilitado no es un e-mail correcto.');
+                break;
             default:
-            alert('Se ha producido un error al crear el usuario.\n\n'+error+'\n');
-            break;
+                alert('Se ha producido un error al crear el usuario.\n\n' + error + '\n');
+                break;
         }
     }
 }
 
 
-$(function()
-{
+$(function() {
     // Programamos el click de los botones del formulario:
-    $("#botonRegistro").click(function()
-    {
-        email=$("#email").val();
-        password=$("#password").val();
-        passwordConfirm=$("#password2").val();
+    $("#botonRegistro").click(function() {
+        email = $("#email").val();
+        password = $("#password").val();
+        passwordConfirm = $("#password2").val();
 
-        if (password != passwordConfirm)
-        {
+        if (password != passwordConfirm) {
             alert("Error: Las contraseñas son distintas!");
-        }
-        else
-            firebase.auth().createUserWithEmailAndPassword(email,password).then(exito).catch(alFinalizar);
+        } else
+            firebase.auth().createUserWithEmailAndPassword(email, password).then(exito).catch(alFinalizar);
     });
 
 
-    $("#botonCancelar").click(function()
-    {
+    $("#botonCancelar").click(function() {
         location.assign('index.html');
     });
 
